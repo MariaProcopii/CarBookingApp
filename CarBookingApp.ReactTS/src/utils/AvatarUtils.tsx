8003743b0ab32c3e3df6dda2e151a82f75241130
MariaProcopii <mari.procopii@gmail.com>
2024-06-19 15:05:15 +0300

    Add assets and utils [refactor]

---
+++ CarBookingApp.ReactTS/src/utils/AvatarUtils.tsx
@@ -0,0 +1,19 @@
+const avatarImages = {
+    MALE: "src/assets/images/profile-img-man.png",
+    FEMALE: "src/assets/images/profile-img-woman.png",
+    OTHER: "src/assets/images/profile-img-other.png",
+    DEFAULT: "src/assets/images/profile-img-man.png"
+};
+
+export default function getAvatarSrc (gender: string) {
+    switch (gender) {
+        case "MALE":
+            return avatarImages.MALE;
+        case "FEMALE":
+            return avatarImages.FEMALE;
+        case "OTHER":
+            return avatarImages.OTHER;
+        default:
+            return avatarImages.DEFAULT;
+    }
+};
